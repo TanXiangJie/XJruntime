cac515ccc2a33a17b6b16dfae747f400f4208351
谭向捷 <mktanxiangjie@163.com>
2015-07-21 18:52:46 +0800

    Initial Commit

---
+++ XJruntime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XJruntime
+//
+//  Created by 若水三千 on 15/7/21.
+//  Copyright (c) 2015年 若水三千. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
